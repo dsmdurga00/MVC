5ea47e28c749f8fcbcb638ffaf7a3cf9c1aa00a7
dsmdurga00 <84117977+dsmdurga00@users.noreply.github.com>
2025-08-26 13:34:08 +0530

    Add files via upload

---
+++ java/com/durga/model/User.java
@@ -0,0 +1,27 @@
+package com.durga.model;
+
+public class User {
+	
+	private String username;
+	private String email;
+	private String city;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+}
