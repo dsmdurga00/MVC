11ee443e2d55e604eb84a7c1d44ffc8643a9275a
dsmdurga00 <84117977+dsmdurga00@users.noreply.github.com>
2025-08-26 13:33:31 +0530

    Add files via upload

---
+++ java/com/durga/dbConn/DbConnection.java
@@ -0,0 +1,24 @@
+package com.durga.dbConn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbConnection {
+
+	public static Connection getConnection() {
+		
+		Connection con = null;
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mvc","root","Pass@123");
+		} 
+		catch (Exception e) 
+		{
+               e.printStackTrace();
+               
+		}
+		
+		return con;
+	}
+}
